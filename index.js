d6bca03efdef499a89243ec48d71377c635a2a08
David Ongaro <davidongaroh@gmil.com>
2022-06-06 16:53:51 +0300

    commit tree 1afe7541b84584cf9921b09849c16ae07a8ca4f6

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser="You need to modify the value of the 'currentUser' variable"
+let welcomeMessage= "Welcome to Flatbook, You need to modify the value of the 'currentUser' variable!"
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+let excitedWelcomeMessage= `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+let shortGreeting= 'Welcome, Y!'
